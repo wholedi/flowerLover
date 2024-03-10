4c1115d6e2d2d041ed8be9a7adc8d92d536bb78b
Taras <131402561+wholedi@users.noreply.github.com>
2024-03-10 21:41:00 +0200

    creat filter

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
+"use strict"
 const icon = document.querySelector('.icon-menu');
 icon.addEventListener('click', function () {
     document.documentElement.classList.toggle('menu-open');
 })
+
+const categoryItems = document.querySelectorAll('.categories__item');
+categoryItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // Получаем индексы из data-атрибутов
+        const filterIndex = item.dataset.filterIndex;
+        const buttonIndex = item.dataset.buttonIndex;
+
+        // // Получаем все элементы с классом "filter" и удаляем класс "filter-open" у всех
+        const filterElements = document.querySelectorAll('.filter');
+        filterElements.forEach(filter => filter.classList.remove('filter-open'));
+
+        // Добавляем класс "filter-open" только к соответствующему элементу по индексу
+        filterElements[filterIndex].classList.add('filter-open');
+
+        // Получаем все элементы button и удаляем класс "open-filter" у всех
+        const buttons = document.querySelectorAll('.filter__cross');
+        buttons.forEach(button => button.classList.remove('open-filter'));
+
+        // Добавляем класс "open-filter" только к соответствующей кнопке по индексу
+        buttons[buttonIndex].classList.add('open-filter');
+    });
+});
