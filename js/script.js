25ee5d21822cf467bf1fbfa4dc72e47ad47ee3eb
Diana <kononyhina404@gmail.com>
2024-03-11 21:51:23 +0200

    creat adaptive-filter

--- js/script.js
+++ js/script.js
@@ -5,24 +5,130 @@ icon.addEventListener('click', function () {
 })
 
 const categoryItems = document.querySelectorAll('.categories__item');
-categoryItems.forEach(item => {
-    item.addEventListener('click', () => {
-        // Получаем индексы из data-атрибутов
-        const filterIndex = item.dataset.filterIndex;
-        const buttonIndex = item.dataset.buttonIndex;
-
-        // // Получаем все элементы с классом "filter" и удаляем класс "filter-open" у всех
-        const filterElements = document.querySelectorAll('.filter');
-        filterElements.forEach(filter => filter.classList.remove('filter-open'));
-
-        // Добавляем класс "filter-open" только к соответствующему элементу по индексу
-        filterElements[filterIndex].classList.add('filter-open');
-
-        // Получаем все элементы button и удаляем класс "open-filter" у всех
-        const buttons = document.querySelectorAll('.filter__cross');
-        buttons.forEach(button => button.classList.remove('open-filter'));
-
-        // Добавляем класс "open-filter" только к соответствующей кнопке по индексу
-        buttons[buttonIndex].classList.add('open-filter');
-    });
+categoryItems.forEach(category => {
+    const categoriesButton = category.querySelector(".categories__button")
+    const dropdownFiltersList = category.querySelector(".filter")
+    const buttonCross =  dropdownFiltersList.querySelector(".filter__cross");
+    const buttonFilter =  dropdownFiltersList.querySelector(".filter__button");
+    categoriesButton.addEventListener('click', function () {
+        dropdownFiltersList.classList.toggle('filter-open');
+    })
+    buttonCross.addEventListener('click', function () {
+        dropdownFiltersList.classList.remove('filter-open');
+    })
+    buttonFilter.addEventListener('click', function () {
+        dropdownFiltersList.classList.remove('filter-open');
+    })
 });
+
+document.addEventListener("click", documentActions);
+function documentActions(e) {
+    const targetElement = e.target;
+    if (targetElement.closest('[data-spoller]')) {
+        const currentElement = targetElement.closest('[data-spoller]');
+        if (!currentElement.nextElementSibling.classList.contains('--sliding')) {
+            currentElement.classList.toggle('active');
+        }
+        slideToggle(currentElement.nextElementSibling);
+    }
+}
+const spollers = document.querySelectorAll('[data-spoller]')
+if (spollers.length) {
+    spollers.forEach(spoller => {
+        spoller.dataset.spoller !== 'open' ? spoller.nextElementSibling.hidden = true : spoller.classList.add('active')
+    });
+    // Filter
+    const filterTitle = document.querySelector('.filter__title')
+    if (filterTitle) {
+        //window.addEventListener('resize', someFunc);
+        const breakPointValue = filterTitle.dataset.spollerMedia;
+        const breakPoint = breakPointValue ? `(${breakPointValue.split(',')[0]}-width:${breakPointValue.split(',')[1]}px)` : null
+        if (breakPoint) {
+            const matchMedia = window.matchMedia(breakPoint)
+            matchMedia.addEventListener("change", (e) => {
+                const isTrue = e.matches
+                if (isTrue) {
+                    slideUp(filterTitle.nextElementSibling)
+                    filterTitle.classList.remove('active')
+                } else {
+                    slideDown(filterTitle.nextElementSibling)
+                    filterTitle.classList.add('active')
+                }
+            })
+        }
+    }
+}
+
+let slideDown = (target, duration = 500) => {
+    if (!target.classList.contains('--sliding')) {
+        target.classList.add('--sliding');
+        target.hidden = false;
+        let height = target.offsetHeight;
+
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+
+        target.offsetHeight;
+
+        target.style.transitionProperty = `height, margin, padding`;
+        target.style.transitionDuration = `${duration}ms`;
+
+        target.style.height = `${height}px`;
+
+        target.style.removeProperty('padding-top')
+        target.style.removeProperty('padding-bottom')
+        target.style.removeProperty('margin-bottom')
+        target.style.removeProperty('margin-top')
+
+        setTimeout(() => {
+            target.style.removeProperty('height')
+            target.style.removeProperty('overflow')
+            target.style.removeProperty('transition-duration')
+            target.style.removeProperty('transition-property')
+            target.classList.remove('--sliding');
+        }, duration);
+    }
+}
+let slideUp = (target, duration = 500) => {
+    if (!target.classList.contains('--sliding')) {
+        target.classList.add('--sliding');
+        let height = target.offsetHeight;
+
+        target.style.transitionProperty = `height, margin, padding`;
+        target.style.transitionDuration = `${duration}ms`;
+        target.style.height = `${height}px`;
+
+        target.offsetHeight;
+
+        target.style.overflow = 'hidden';
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+
+        target.style.height = 0;
+
+        setTimeout(() => {
+            target.style.removeProperty('padding-top')
+            target.style.removeProperty('padding-bottom')
+            target.style.removeProperty('margin-bottom')
+            target.style.removeProperty('margin-top')
+
+            target.style.removeProperty('height')
+            target.style.removeProperty('overflow')
+            target.style.removeProperty('transition-duration')
+            target.style.removeProperty('transition-property')
+            target.classList.remove('--sliding');
+
+            target.hidden = true;
+        }, duration);
+    }
+}
+let slideToggle = (target, duration = 500) => {
+    target.hidden ? slideDown(target, duration) : slideUp(target, duration)
+}
